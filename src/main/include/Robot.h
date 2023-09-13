7473be2854399614e51c9b61b7e89c483b2fb6ee
EdisonSchool <edison.compton.mtv@scienceandtech.org>
2023-09-13 16:03:47 -0600

    init

---
+++ src/main/include/Robot.h
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+
+#include <rev/CANSparkMax.h>
+
+#include <frc/XboxController.h>
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  rev::CANSparkMax motor {2, rev::CANSparkMaxLowLevel::MotorType::kBrushed};
+  frc::XboxController stick {0};
+
+  rev::CANSparkMax m_left_motor {0, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+  rev::CANSparkMax m_right_motor {1, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+
+
+};
